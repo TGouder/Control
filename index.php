e246e3a9a200008bad1943105b2a8aeaedac003b
Tom Gouder <me@tomgouder.com>
2014-07-01 10:31:45 +0200

    Landing Page
    Started to design the landing page and other artefacts.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Control
+		</title>
+		<link rel="stylesheet" type="text/css" href="css/landing/base.css">
+		<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+	</head>
+	<body>
+		<div id="horizon">
+			<div id="grid">
+				<div id="layer">
+				</div>
+				<div id="container">
+					<div id="text-logo">
+					</div>
+					<div id="form">
+						<input type="text" placeholder="Control ID"></input>
+						<input type="password" placeholder="Control ID"></input>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
